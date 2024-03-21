eed480119d318c82f54e69041c03ee3520d49689
brian-ambani <brainambani1@gmail.com>
2024-03-21 15:32:15 +0300

    using usecontext hook

--- hooks/src/components/ComponentB.jsx
+++ hooks/src/components/ComponentB.jsx
@@ -1,9 +1,14 @@
+import React, { useContext } from "react";
 import ComponentC from "./ComponentC";
+import { UserContext, ChannelContext } from "../App";
 
 export default function ComponentB() {
+  const user = useContext(UserContext);
+  const channel = useContext(ChannelContext);
+
   return (
     <div>
-      <ComponentC />
+      {user} - {channel}
     </div>
   );
 }
